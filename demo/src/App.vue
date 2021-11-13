d22266efc8a501ed819d97d9f59da3e4357ae62f
Johnson Chen <chnejohnson@gmail.com>
2021-11-13 09:06:11 +0800

    demo: change to <script setup>

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,5 +1,4 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import {
   useBoard,
   useEthers,
@@ -9,45 +8,24 @@ import {
   shortenAddress,
 } from 'vue-dapp'
 
-export default defineComponent({
-  name: 'App',
-  setup() {
-    const { open } = useBoard()
-    const { status, disconnect, error } = useWallet()
-    const { address, balance, chainId, isActivated } = useEthers()
-
-    return {
-      isActivated,
-      address,
-      status,
-      error,
-      chainId,
-      balance,
-      open,
-      disconnect,
-      displayEther,
-      displayChainName,
-      shortenAddress,
-    }
-  },
-})
+const { open } = useBoard()
+const { status, disconnect, error } = useWallet()
+const { address, balance, chainId, isActivated } = useEthers()
 </script>
 
 <template>
   <div class="h-full flex flex-col justify-center items-center">
+    <p v-if="error" class="text-red-500">{{ error }}</p>
 
-    <p
-      v-if="error"
-      class="text-red-500"
-    >{{ error }}</p>
-
-    <div
-      v-if="isActivated"
-      class="text-center"
-    >
+    <div v-if="isActivated" class="text-center">
       <p>{{ shortenAddress(address) }}</p>
       <p>{{ displayEther(balance) }} ETH</p>
-      <p>network: <span class="capitalize"> {{ chainId ? displayChainName(chainId) : '' }} </span></p>
+      <p>
+        network:
+        <span class="capitalize">
+          {{ chainId ? displayChainName(chainId) : '' }}
+        </span>
+      </p>
     </div>
 
     <div class="m-4">
@@ -55,7 +33,15 @@ export default defineComponent({
         @click="isActivated ? disconnect() : open()"
         class="btn"
         :disabled="status === 'connecting'"
-      >{{ status === 'connected' ? "Disconnect" : status === 'connecting' ? "Connecting..." : "Connect" }}</button>
+      >
+        {{
+          status === 'connected'
+            ? 'Disconnect'
+            : status === 'connecting'
+            ? 'Connecting...'
+            : 'Connect'
+        }}
+      </button>
     </div>
   </div>
   <vdapp-board />
