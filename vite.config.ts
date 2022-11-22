e9b6fff235ec4f786f0409dcb2671d13bd460dcd
Johnson <chnejohnson@gmail.com>
2022-11-22 22:20:25 +0800

    Bump vue, vite, and @vitejs/plugin-vue to latest version, and resolve polyfill node with vite vai new configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,40 @@
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 import WindiCSS from 'vite-plugin-windicss'
-import inject from '@rollup/plugin-inject'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   root: 'demo/',
   plugins: [vue(), WindiCSS()],
   resolve: {
     alias: {
-      'vue-dapp': resolve(__dirname, './src/index.ts'),
+      'vue-dapp': path.resolve(__dirname, './src/index.ts'),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins, refer to https://stackoverflow.com/a/72440811/10752354
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
   build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-    },
     commonjsOptions: {
-      transformMixedEsModules: true,
+      transformMixedEsModules: true, // @walletconnect/web3-provider is CommonJS, so need to open this
+    },
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin used during production bundling, refer to https://stackoverflow.com/a/72440811/10752354
+        nodePolyfills(),
+      ],
     },
   },
 })
