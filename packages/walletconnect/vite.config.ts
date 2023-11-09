7cb31d3ec5f24628f70bb75df3dfb02e5a3f481b
Johnson Chen <chnejohnson@gmail.com>
2023-11-09 14:57:52 +0800

    feat: publish @vue-dapp/walletconnect@1.0.0-alpha.2

---
+++ packages/walletconnect/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json'
+
+const name = pkg.name.split('/').pop()
+
+export default defineConfig({
+	plugins: [
+		vue(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	build: {
+		assetsDir: 'assets',
+		// Vite Library Mode https://vitejs.dev/guide/build.html#library-mode
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: name,
+			fileName: name,
+		},
+		outDir: 'dist',
+		rollupOptions: {
+			external: ['vue', 'pinia'],
+			output: {
+				dir: 'dist',
+				globals: {
+					vue: 'vue',
+					pinia: 'pinia',
+				},
+			},
+		},
+	},
+})
