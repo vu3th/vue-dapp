a797e32ea62c7647ab75783d5b0eb330ecf0e9b1
Johnson Chen <chnejohnson@gmail.com>
2024-01-04 21:21:32 +0800

    refactor: refactor Board.vue with setup script

--- packages/vd-board/src/App.vue
+++ packages/vd-board/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Board from './components/Board.vue'
 import { useBoardStore } from './stores/useBoardStore'
-import { MetaMaskConnector } from '@vue-dapp/core'
+import { MetaMaskConnector, useVueDapp } from '@vue-dapp/core'
 
 const connectors = [new MetaMaskConnector()]
 
@@ -12,12 +12,25 @@ function autoConnectErrorHandler(err: any) {
 	console.error('AutoConnectError', err)
 }
 
-const { open } = useBoardStore()
+const boardStore = useBoardStore()
+
+const { isConnected, address, chainId, disconnect } = useVueDapp()
+
+function onClickConnectButton() {
+	if (!isConnected.value) {
+		boardStore.open()
+	} else {
+		disconnect()
+	}
+}
 </script>
 
 <template>
 	<div>
-		<button @click="open">Connect</button>
+		<button @click="onClickConnectButton">{{ isConnected ? 'Disconnect' : 'Connect Wallet' }}</button>
+		<p v-if="chainId !== -1">Chain ID: {{ chainId }}</p>
+		<p>{{ address }}</p>
+
 		<Board
 			:connectors="connectors"
 			dark
